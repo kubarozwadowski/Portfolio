ba302a6dcf4fdff49837d2b14862dbe69f75b12f
Kuba Rozwadowski <officialkubarozwadowski@gmail.com>
2025-08-27 12:10:15 -0400

    Updated country counter

--- pofology/src/components/partials/AboutSection.tsx
+++ pofology/src/components/partials/AboutSection.tsx
@@ -92,7 +92,7 @@ const AboutSection = () => {
             <HiOutlineUsers size={50} />
           </div>
           <div>
-            <h2 className="text-3xl font-bold">14+</h2>
+            <h2 className="text-3xl font-bold">17</h2>
             <p className="mt-1">Countries visited</p>
           </div>
         </div>
